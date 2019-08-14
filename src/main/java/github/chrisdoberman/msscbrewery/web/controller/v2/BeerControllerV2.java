3d9aa93a1f05cd81bcfadefffdbbdd1e0db4c56e
chrisdoberman <chris.herb@gmail.com>
2019-08-13 20:06:05 -0400

    add lombok examples

--- src/main/java/github/chrisdoberman/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/github/chrisdoberman/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -2,6 +2,9 @@ package github.chrisdoberman.msscbrewery.web.controller.v2;
 
 import github.chrisdoberman.msscbrewery.services.v2.BeerServiceV2;
 import github.chrisdoberman.msscbrewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +13,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerService;
-
-    public BeerControllerV2(BeerServiceV2 beerService) {
-        this.beerService = beerService;
-    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -28,7 +29,8 @@ public class BeerControllerV2 {
     @PostMapping // POST - create new beer
     public ResponseEntity handlePost(@Valid @RequestBody BeerDtoV2 beerDto) {
 
-        BeerDtoV2 savedDto = beerService.saveNewBeer(beerDto);
+        log.debug("in handlePost method.");
+        val savedDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         //TODO: add hostname to url
